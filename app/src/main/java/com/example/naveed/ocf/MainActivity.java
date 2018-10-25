72007f1da76439610736f128f0b3324c6f70465d
Muhammad Naveed <naveed2309923@hotmail.com>
2018-10-25 07:18:51 -0700

    My Order

--- app/src/main/java/com/example/naveed/ocf/MainActivity.java
+++ app/src/main/java/com/example/naveed/ocf/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.order_activity);
+        setContentView(R.layout.my_order);
     }
 }
